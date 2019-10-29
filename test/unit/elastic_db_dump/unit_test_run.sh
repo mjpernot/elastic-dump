02ba48bad6ba120afd8fee22650717257aa7b776
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-29 16:16:34 +0000

    Update test/unit/elastic_db_dump/unit_test_run.sh

--- test/unit/elastic_db_dump/unit_test_run.sh
+++ test/unit/elastic_db_dump/unit_test_run.sh
@@ -5,34 +5,13 @@
 #   is located at.
 
 echo ""
-echo "Unit test:  help_message"
+echo "Unit test:"
 test/unit/elastic_db_dump/help_message.py
-
-echo ""
-echo "Unit test:  print_failures"
 test/unit/elastic_db_dump/print_failures.py
-
-echo ""
-echo "Unit test:  create_repo"
 test/unit/elastic_db_dump/create_repo.py
-
-echo ""
-echo "Unit test:  initate_dump"
 test/unit/elastic_db_dump/initate_dump.py
-
-echo ""
-echo "Unit test:  list_dumps"
 test/unit/elastic_db_dump/list_dumps.py
-
-echo ""
-echo "Unit test:  list_repos"
 test/unit/elastic_db_dump/list_repos.py
-
-echo ""
-echo "Unit test:  run_program"
 test/unit/elastic_db_dump/run_program.py
-
-echo ""
-echo "Unit test:  main"
 test/unit/elastic_db_dump/main.py
 
