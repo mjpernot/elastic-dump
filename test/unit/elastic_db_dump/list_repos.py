2b082fc733bf93e18c524f4c9ecd3498acf36622
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-11-22 15:33:32 +0000

    Update test/unit/elastic_db_dump/list_repos.py v1.1.0

--- test/unit/elastic_db_dump/list_repos.py
+++ test/unit/elastic_db_dump/list_repos.py
@@ -41,8 +41,8 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Unit testing initilization.
-        test_list_repos -> Test the printing of respositories.
+        setUp
+        test_list_repos
 
     """
 
@@ -63,7 +63,7 @@ class UnitTest(unittest.TestCase):
             Description:  Class representation of the ElasticSearchDump class.
 
             Methods:
-                __init__ -> Initialize configuration environment.
+                __init__
 
             """
 
@@ -89,7 +89,7 @@ class UnitTest(unittest.TestCase):
             Description:  Class representation of the ElasticSearchRepo class.
 
             Methods:
-                __init__ -> Initialize configuration environment.
+                __init__
 
             """
 
